2eca07bfddd1af2ca7f273350b05dba5dcb81896
Ritika Sahay <ritikasahay10@gmail.com>
2024-09-14 15:20:05 +0530

    Initial commit

--- Server/App.jsx
+++ Server/App.jsx
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
 const userModel = require('./models/user.jsx'); // Assuming you have a Mongoose model set up
